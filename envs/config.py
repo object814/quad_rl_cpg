c6e98d82e0234314f04b7fa5bee3cca60b1fcd02
object814 <zhouhy0108@gmail.com>
2024-11-21 22:07:25 +0800

    update config

--- envs/config.py
+++ envs/config.py
@@ -9,24 +9,24 @@ dt = 0.01                                   # Time step for updating the CPG sta
 # animate_cpg = True                          # Whether to animate the CPGs
 discount_factor = 1.0                      # Discount factor per timestep for total rewards
 # fixed_base = False                          # Whether to fix the base of the robot
-reset_position = [0, 0, 0.4]                # Initial position of the robot
+reset_position = [0, 0, 0.3]                # Initial position of the robot
 
 
 # Reward / penalty weights
-forward_progress_reward_weight = 100.0        # Reward weight for forward progress, distance travelled from the starting position
-roll_stability_penalty_weight = 1.0         # Penalty weight for roll stability
-pitch_stability_penalty_weight = 1.0        # Penalty weight for pitch stability
-payload_drop_penalty_weight = 10.0           # Penalty weight for payload drop
+forward_progress_reward_weight = 300.0        # Reward weight for forward progress, distance travelled from the starting position
+roll_stability_penalty_weight = 10.0         # Penalty weight for roll stability
+pitch_stability_penalty_weight = 10.0        # Penalty weight for pitch stability
+payload_drop_penalty_weight = 50000.0           # Penalty weight for payload drop
 foot_slip_penalty_weight = 0              # Penalty weight for foot slip
 
 
 # Penalty thresholds
-max_roll = 0.6                              # in radians, threashold for simulating weight dropped
-min_roll = -0.6                             # in radians, threashold for simulating weight dropped
-max_pitch = 0.6                             # in radians, threashold for simulating weight dropped
-min_pitch = -0.6                            # in radians, threashold for simulating weight dropped
-roll_pitch_range_without_penalty = 0.1745   #  in radians, range for robot base orientation in roll and pitch which won't be penalized
-roll_pitch_penalty_threashold = roll_pitch_range_without_penalty ** 2
+max_roll = 0.8                              # in radians, threashold for simulating weight dropped
+min_roll = -0.8                             # in radians, threashold for simulating weight dropped
+max_pitch = 0.8                             # in radians, threashold for simulating weight dropped
+min_pitch = -0.8                            # in radians, threashold for simulating weight dropped
+roll_pitch_range_without_penalty = 0.4   #  in radians, range for robot base orientation in roll and pitch which won't be penalized
+roll_pitch_penalty_threashold = roll_pitch_range_without_penalty
 
  # Added box params
 add_weight = True
